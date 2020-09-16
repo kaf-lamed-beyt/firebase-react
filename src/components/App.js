1296a97923d12e6e7bdbb8f38c03cad1a0c9b6a0
Caleb335 <belac335@gmail.com>
2020-09-16 16:24:12 +0100

    fix routing in components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,36 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Navigation from './Navigation'
+import LandingPage from './Welcome'
+import SignUpPage from './Signup'
+import SignInPage from './LogIn'
+import UserProfile from './User'
+import Home from './HomePage'
+import ForgotPassword from './ForgotPwd'
+import Admin from './Admin.js'
 
-const App = () => {
+import * as Routes from '../Routes/routes'
+
+class App extends React.Component {
+  render() {
     return (
-      <div className="app__base">
-        <h1>App base</h1>
-        <Navigation />
-
-        <h2>Please</h2>
-        <hr />
-      </div>
+      <Router>
+        <div className="app__base">
+          <hr />
+          {/* routes */}
+          <Route exact path={Routes.landing} component={LandingPage} />
+          <Route path={Routes.signup} component={SignUpPage} />
+          <Route path={Routes.login} component={SignInPage} />
+          <Route path={Routes.profile} component={UserProfile} />
+          <Route path={Routes.home} component={Home} />
+          <Route path={Routes.forgotPassword} component={ForgotPassword} />
+          <Route path={Routes.admin} component={Admin} />
+           <Navigation />
+        </div>
+      </Router>
     )
+  }
 }
 
 export default App
