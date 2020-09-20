0988acbd016c7fc2311a61fc9d55bf43cd61121d
Caleb335 <belac335@gmail.com>
2020-09-20 20:22:42 +0100

    God, this shit is annoying aaarrrrrrrggghhhh  😠

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -30,8 +30,8 @@ export default function Login() {
             event={handleChange}
             default_text="password"
           />
+          <LoginBtn className="btn" color="rgb(2, 10, 85)" />
         </form>
-        <LoginBtn className="btn" color="rgb(2, 10, 85)" />
       </div>
     </div>
   )
