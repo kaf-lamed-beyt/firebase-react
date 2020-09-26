39d8ac92b4bb6083e56e0fbb4397c80e876d29a4
Caleb335 <belac335@gmail.com>
2020-09-26 20:25:15 +0100

    add react proptypes for independent components

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const Button = ({ className, color, children }) => {
+export const Button = ({ className, color, children, event }) => {
   return (
-    <button className={className} style={{ background: color }}>
+    <button className={className} style={{ background: color }} onClick={event}>
       {children}
     </button>
   )
+}
+
+Button.PropTypes = {
+  // button label: the text that the button displays
+  children: PropTypes.node.isRequired,
+  // the color prop of the button
+  color: PropTypes.string,
+  // element class for unique styling
+  className: PropTypes.string,
+  // click event for the button that gets called when it is clicked
+  onClick: PropTypes.func
 }
--- src/components/SocialAuth.js
+++ src/components/SocialAuth.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const SocialAuth = ({ children }) => {
   return <div className="social__signin">{children}</div>
+}
+
+SocialAuth.PropTypes = {
+  children: PropTypes.children.isRequired
 }
 
 export default SocialAuth
