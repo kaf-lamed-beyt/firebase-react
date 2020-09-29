b478bd039953021407f8e9662d491fff2642bacc
Caleb335 <belac335@gmail.com>
2020-09-29 19:33:50 +0100

    encapsulate web key

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -2,12 +2,12 @@ import app from 'firebase/app'
 import 'firebase/auth'
 
 const config = {
-    apiKey: "AIzaSyCz5OSJATR7fdcgrBvbgElIic55eRy67lA",
-    authDomain: process.env.MY_AUTH_DOMAIN,
-    databaseURL: process.env.MY_DATABASE_URL,
-    projectId: process.env.MY_PROJECT_ID,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.MY_MESSAGING_SENDER_ID
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 }
 
 class Firebase {
